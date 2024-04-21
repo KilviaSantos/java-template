c3e3dd21a39d81a42b83e25897094d7a815da92c
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2024-04-21 19:58:32 +0000

    :memo: chore: create question5

---
+++ app/src/main/java/com/atv1/app/question5.java
@@ -0,0 +1,14 @@
+package com.atv1.app;
+
+/* 
+1 - Elaborar um algoritmo que leia um número inteiro positivo n;
+2 - Preencher com valores gerados aleatoriamente no intervalo de 1 a 2n uma lista com n elementos;
+3 - Organizar os valores em ordem crescente.  
+*/
+
+public class question5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
