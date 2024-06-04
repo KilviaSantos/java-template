3d9bd0730cb236cdd28ef4e908fb0a8350097fb6
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2024-06-04 19:28:27 +0000

    :tada: feat: create file LingP2_Pessoa.java

---
+++ app/src/main/java/com/atv1/app/linguagemProg2/LingP2_Pessoa.java
@@ -0,0 +1,18 @@
+package com.atv1.app.linguagemProg2;
+
+
+public class LingP2_Pessoa {
+    private String nome;
+    private int idade;
+    private boolean mulher;
+    private float altura;
+
+    public LingP2_Pessoa(String nome, int idade, boolean mulher, float altura){
+        this.nome = nome;
+        this.idade = idade;
+        this.mulher = mulher;
+        this.altura = altura;
+
+    }
+
+}
